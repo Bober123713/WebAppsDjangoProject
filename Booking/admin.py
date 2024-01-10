0cd4264a5b45b2d384b912f9504916865640cb3f
Michał Bober <michalbober.lublin@gmail.com>
2024-01-10 22:16:17 +0100

    Custom admin view for bookings

--- Booking/admin.py
+++ Booking/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Booking
 
-# Register your models here.
-admin.site.register(Booking)
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'car', 'start_date', 'end_date', 'transmission_type', 'price', 'gps_service')
+    list_filter = ('start_date', 'end_date', 'car', 'transmission_type', 'gps_service')
+    search_fields = ('user__username', 'car__make', 'car__model', 'transmission_type')
+    ordering = ('start_date', 'end_date', 'user', 'car')
+
+
+admin.site.register(Booking, BookingAdmin)
